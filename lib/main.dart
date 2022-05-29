72323783edb22beca2226076314621e6e4ca517d
Yi Xin Ting <yxting1109@gmail.com>
2022-05-30 01:16:56 +0800

    Fix main.dart firebase edits

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,40 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:purrfect_compawnion/pages/authenticate/login.dart';
+import 'package:purrfect_compawnion/pages/authenticate/register.dart';
 import 'package:purrfect_compawnion/pages/features/home.dart';
 import 'package:purrfect_compawnion/pages/features/pethouse.dart';
 import 'package:purrfect_compawnion/pages/features/welcome.dart';
+import 'package:purrfect_compawnion/pages/wrapper.dart';
+import 'package:purrfect_compawnion/services/auth.dart';
 
-
-void main() {
-  runApp(MaterialApp(
-    initialRoute: '/welcome',
-
-    routes: {
-      '/': (context) => Login(),
-      '/home': (context) => Home(),
-      '/pethouse': (context) => PetHouse(),
-      '/welcome' : (context) => Welcome(),
-    },
-  ));
+import 'models/myuser.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<MyUser?>.value(
+      initialData: null,
+      value: AuthService().user,
+      child: MaterialApp(
+        initialRoute: '/welcome',
+        routes: {
+          '/': (context) => Wrapper(),
+          '/welcome': (context) => Welcome(),
+          '/login': (context) => Login(),
+          '/register': (context) => Register(),
+          '/home': (context) => Home(),
+          '/pethouse': (context) => PetHouse(),
+        },
+      ),
+    );
+  }
 }
--- lib/pages/features/welcome.dart
+++ lib/pages/features/welcome.dart
@@ -5,7 +5,8 @@ class Welcome extends StatefulWidget {
   const Welcome({Key? key}) : super(key: key);
 
   @override
-  _WelcomeState createState() => _WelcomeState();}
+  _WelcomeState createState() => _WelcomeState();
+}
 
 class _WelcomeState extends State<Welcome> {
   @override
@@ -14,166 +15,157 @@ class _WelcomeState extends State<Welcome> {
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
         backgroundColor: Colors.red[100],
-        body:
-        SingleChildScrollView(
-          child:
-          Column(
-              children: [
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Padding(
-                      padding: const EdgeInsets.only(left: 0, top: 0),
-                      child:
-                      Container(
-                          width: 415,
-                          height: 72,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(0),
-                          ),
-                          child: Stack(
-                            children: [
-                              Align(
-                                alignment: Alignment.topLeft,
-                                child: Padding(
-                                    padding: const EdgeInsets.only(left: 27, top: 14),
-                                    child:
-                                    Container(
-                                        width: 79,
-                                        height: 46,
-                                        decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(24),
-                                        ),
-                                        child: Stack(
-                                          children: [
-                                            Align(
-                                              alignment: Alignment.topLeft,
-                                              child: Padding(
-                                                  padding: const EdgeInsets.only(top: 0, left: 0),
-                                                  child:
-                                                  Container(
-                                                      width: 79,
-                                                      height: 46,
-                                                      decoration: BoxDecoration(
-                                                        borderRadius: BorderRadius.circular(24),
-                                                      ),
-                                                      child: Stack(
-                                                        children: [
-                                                          Align(
-                                                            alignment: Alignment.topLeft,
-                                                            child:   const Padding(
-                                                              padding: const EdgeInsets.only(left: 0, top: 1),
-                                                            ),
-
-                                                          ),
-                                                        ],
-                                                      ))),
-
-                                            ),
-                                          ],
-                                        ))),
-
-                              ),
-                              Align(
-                                alignment: Alignment.topLeft,
-                                child:   Padding(
-                                  padding: const EdgeInsets.only(left: 286, top: 19),
-
-                                ),
-
-                              ),
-                            ],
-                          ))),
-
-                ),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child:   const Padding(
-                      padding: const EdgeInsets.only(left: 23, top: 28),
-                      child:
-                      Text(
-                        "It’s time to meet your new",
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                          fontSize: 32,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      )),
-
-                ),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child:   const Padding(
-                      padding: const EdgeInsets.only(left: 23, top: 10),
-                      child:
-                      Text(
-                        "Bestfriend",
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                          fontSize: 70,
-                          color: Colors.redAccent,
-                          fontWeight: FontWeight.w400,
-                          fontFamily: 'StyleScript',
-                        ),
-                      )),
-
-                ),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child:   Padding(
-                    padding: const EdgeInsets.only(left: 23, top: 98),
-                    child: Container(
-                      width: 180,
-                      height: 58,
+        body: SingleChildScrollView(
+          child: Column(children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                  padding: const EdgeInsets.only(left: 0, top: 0),
+                  child: Container(
+                      width: 415,
+                      height: 72,
                       decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage("assets/Frame_ImageView_16-180x58.png")
-                        ),
+                        borderRadius: BorderRadius.circular(0),
                       ),
-                      child: TextButton(
-                        onPressed: () {
-                          Navigator.pushReplacementNamed(context, '/');
-                        }, child: Text(""),
-                      ),
-                    ),
-                  ),
-
-                ),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child:   Padding(
-                    padding: const EdgeInsets.only(left: 7, top: 76),
-                    child: SizedBox(
-                      height: 337,
-                      width: 433,
-                      child: Image.asset("assets/LoginSoccatCropped_ImageView_17-433x337.png"),
-                    ),
-                  ),
-
-                ),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Padding(
-                      padding: const EdgeInsets.only(left: 7, top: 16),
-                      child:
-                      Container(
-                          width: width,
-                          height: 59,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(0),
+                      child: Stack(
+                        children: [
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: Padding(
+                                padding:
+                                    const EdgeInsets.only(left: 27, top: 14),
+                                child: Container(
+                                    width: 79,
+                                    height: 46,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(24),
+                                    ),
+                                    child: Stack(
+                                      children: [
+                                        Align(
+                                          alignment: Alignment.topLeft,
+                                          child: Padding(
+                                              padding: const EdgeInsets.only(
+                                                  top: 0, left: 0),
+                                              child: Container(
+                                                  width: 79,
+                                                  height: 46,
+                                                  decoration: BoxDecoration(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            24),
+                                                  ),
+                                                  child: Stack(
+                                                    children: [
+                                                      Align(
+                                                        alignment:
+                                                            Alignment.topLeft,
+                                                        child: const Padding(
+                                                          padding:
+                                                              const EdgeInsets
+                                                                      .only(
+                                                                  left: 0,
+                                                                  top: 1),
+                                                        ),
+                                                      ),
+                                                    ],
+                                                  ))),
+                                        ),
+                                      ],
+                                    ))),
                           ),
-                          child: Stack(
-                            children: [
-                              Align(
-                                alignment: Alignment.topLeft,
-                                child:     Container(),
-
-                              ),
-                            ],
-                          ))),
-
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 286, top: 19),
+                            ),
+                          ),
+                        ],
+                      ))),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: const Padding(
+                  padding: const EdgeInsets.only(left: 23, top: 28),
+                  child: Text(
+                    "It’s time to meet your new",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 32,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  )),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: const Padding(
+                  padding: const EdgeInsets.only(left: 23, top: 10),
+                  child: Text(
+                    "Bestfriend",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 70,
+                      color: Colors.redAccent,
+                      fontWeight: FontWeight.w400,
+                      fontFamily: 'StyleScript',
+                    ),
+                  )),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 23, top: 98),
+                child: Container(
+                  width: 180,
+                  height: 58,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image:
+                            AssetImage("assets/Frame_ImageView_16-180x58.png")),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, '/');
+                    },
+                    child: Text(''),
+                  ),
                 ),
-              ]),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 7, top: 76),
+                child: SizedBox(
+                  height: 337,
+                  width: 433,
+                  child: Image.asset(
+                      "assets/LoginSoccatCropped_ImageView_17-433x337.png"),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                  padding: const EdgeInsets.only(left: 7, top: 16),
+                  child: Container(
+                      width: width,
+                      height: 59,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(0),
+                      ),
+                      child: Stack(
+                        children: [
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: Container(),
+                          ),
+                        ],
+                      ))),
+            ),
+          ]),
         ));
   }
 }

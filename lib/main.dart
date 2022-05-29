dfe3fa70ed224f19a0bad528386bac1184401af9
peaol <pearltwe23@gmail.com>
2022-05-30 00:52:50 +0800

    welcome page UI

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:purrfect_compawnion/pages/authenticate/login.dart';
 import 'package:purrfect_compawnion/pages/features/home.dart';
 import 'package:purrfect_compawnion/pages/features/pethouse.dart';
+import 'package:purrfect_compawnion/pages/features/welcome.dart';
 
 
 void main() {
   runApp(MaterialApp(
-    initialRoute: '/pethouse',
+    initialRoute: '/welcome',
 
     routes: {
       '/': (context) => Login(),
       '/home': (context) => Home(),
       '/pethouse': (context) => PetHouse(),
+      '/welcome' : (context) => Welcome(),
     },
   ));
 }

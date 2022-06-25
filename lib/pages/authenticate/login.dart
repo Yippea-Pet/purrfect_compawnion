11eed87688f19ad41818451fa32f2704740834a6
Yi Xin Ting <yxting1109@gmail.com>
2022-06-25 16:17:03 +0800

    Display useful error message for error signing in/signing up

--- lib/pages/authenticate/login.dart
+++ lib/pages/authenticate/login.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:purrfect_compawnion/services/auth.dart';
+import 'package:purrfect_compawnion/shared/loading.dart';
 
 import '../../shared/constants.dart';
 
@@ -24,152 +26,140 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.red[50],
-      // appBar: AppBar(
-      //   backgroundColor: Colors.orange[200],
-      //   elevation: 0.0,
-      //     title: Text('Sign In'),
-      //   actions: <Widget>[
-      //     TextButton.icon(
-      //       icon: Icon(Icons.person),
-      //       label: Text('Register'),
-      //       onPressed: () {
-      //         widget.toggleView();
-      //       },
-      //     ),
-      //   ],
-      // ),
-      body:  SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.fromLTRB(20, 60, 20, 20),
-                child: Image.asset('assets/Logo.png'),
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20.0),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey,
-                      blurRadius: 5.0,
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.red[50],
+            body: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 60, 20, 20),
+                    child: Image.asset('assets/Logo.png'),
                   ),
-                  ]
-                ),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(height: 30.0),
-                      // Form to fill in email
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.9,
-                        child: TextFormField(
-                          decoration:
-                              textInputDecoration.copyWith(hintText: 'Email'),
-                          validator: (val) =>
-                              val!.isEmpty ? "Enter an email" : null,
-                          onChanged: (value) {
-                            setState(() => (email = value));
-                          },
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      // Form to fill in password
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.9,
-                        child: TextFormField(
-                          decoration: textInputDecoration.copyWith(
-                            hintText: 'Password',
-                            suffixIcon: IconButton(
-                                onPressed: () {
-                                  setState(() {
-                                    obscurePassword = !obscurePassword;
-                                  });
-                                },
-                                icon: Icon(Icons.visibility)),
-                            // suffixIcon: Icon(Icons.visibility),
+                  Container(
+                    height: MediaQuery.of(context).size.height,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey,
+                            blurRadius: 5.0,
                           ),
-                          obscureText: obscurePassword,
-                          validator: (val) => val!.length < 6
-                              ? "Enter a password with at least 6 characters"
-                              : null,
-                          onChanged: (value) {
-                            setState(() => (password = value));
-                          },
-                        ),
-                      ),
-                      SizedBox(
-                        height: 50.0,
-                      ),
-                      // Sign in button
-                      SizedBox(
-                        width: 200.0,
-                        height: 50.0,
-                        child: ElevatedButton(
-                            // onPressed function needs to be async because it will
-                            // interact with firebase
-                            style: ButtonStyle(
-                              shape: MaterialStateProperty.all(
-                                  RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30.0),
-                              )),
-                              backgroundColor:
-                                  MaterialStateProperty.all(Colors.pink),
-                            ),
-                            onPressed: () async {
-                              if (_formKey.currentState!.validate()) {
-                                setState(() => loading = true);
-                                dynamic result =
-                                    await _auth.signInWithEmailAndPassword(
-                                        email, password);
-                                if (result == null) {
-                                  setState(() {
-                                    error = 'Error signing in!';
-                                    loading = false;
-                                  });
-                                }
-                              }
-                            },
-                            child: Text(
-                              'Login',
-                              style: TextStyle(
-                                fontSize: 20.0,
-                                color: Colors.white,
-                              ),
-                            )),
-                      ),
-                      Text(
-                        error,
-                        style: TextStyle(
-                          color: Colors.red,
-                          fontSize: 14.0,
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                        ]),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
                         children: <Widget>[
-                          Text("Don't have an account? "),
-                          TextButton(
-                              onPressed: widget.toggleView,
-                              child: Text("Sign Up!")),
+                          SizedBox(height: 30.0),
+                          // Form to fill in email
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.9,
+                            child: TextFormField(
+                              decoration: textInputDecoration.copyWith(
+                                  hintText: 'Email'),
+                              validator: (val) =>
+                                  val!.isEmpty ? "Enter an email" : null,
+                              onChanged: (value) {
+                                setState(() => (email = value));
+                              },
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          // Form to fill in password
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.9,
+                            child: TextFormField(
+                              decoration: textInputDecoration.copyWith(
+                                hintText: 'Password',
+                                suffixIcon: IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        obscurePassword = !obscurePassword;
+                                      });
+                                    },
+                                    icon: Icon(Icons.visibility)),
+                                // suffixIcon: Icon(Icons.visibility),
+                              ),
+                              obscureText: obscurePassword,
+                              validator: (val) => val!.length < 6
+                                  ? "Enter a password with at least 6 characters"
+                                  : null,
+                              onChanged: (value) {
+                                setState(() => (password = value));
+                              },
+                            ),
+                          ),
+                          SizedBox(
+                            height: 50.0,
+                          ),
+                          // Sign in button
+                          SizedBox(
+                            width: 200.0,
+                            height: 50.0,
+                            child: ElevatedButton(
+                                // onPressed function needs to be async because it will
+                                // interact with firebase
+                                style: ButtonStyle(
+                                  shape: MaterialStateProperty.all(
+                                      RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30.0),
+                                  )),
+                                  backgroundColor:
+                                      MaterialStateProperty.all(Colors.pink),
+                                ),
+                                onPressed: () async {
+                                  if (_formKey.currentState!.validate()) {
+                                    try {
+                                      setState(() => loading = true);
+                                      await _auth.signInWithEmailAndPassword(email, password);
+                                    } on FirebaseAuthException catch (e) {
+                                      setState(() {
+                                        error = e.message!;
+                                        loading = false;
+                                      });
+                                    }
+                                  }
+                                },
+                                child: Text(
+                                  'Login',
+                                  style: TextStyle(
+                                    fontSize: 20.0,
+                                    color: Colors.white,
+                                  ),
+                                )),
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          Text(
+                            error,
+                            style: TextStyle(
+                              color: Colors.red,
+                              fontSize: 14.0,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text("Don't have an account? "),
+                              TextButton(
+                                  onPressed: widget.toggleView,
+                                  child: Text("Sign Up!")),
+                            ],
+                          ),
                         ],
                       ),
-                    ],
+                    ),
                   ),
-                ),
+                ],
               ),
-            ],
-          ),
-        ),
-
-    );
+            ),
+          );
   }
 }
--- lib/pages/authenticate/register.dart
+++ lib/pages/authenticate/register.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:purrfect_compawnion/services/auth.dart';
+import 'package:purrfect_compawnion/shared/loading.dart';
 
 import '../../shared/constants.dart';
 
@@ -24,148 +26,136 @@ class _RegisterState extends State<Register> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.red[50],
-      // appBar: AppBar(
-      //   backgroundColor: Colors.orange[200],
-      //   elevation: 0.0,
-      //   title: Text('Register'),
-      //   actions: <Widget>[
-      //     TextButton.icon(
-      //       icon: Icon(Icons.person),
-      //       label: Text('Sign In'),
-      //       onPressed: () {
-      //         widget.toggleView();
-      //       },
-      //     ),
-      //   ],
-      // ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 60, 20, 20),
-              child: Image.asset('assets/Logo.png'),
-            ),
-            Container(
-              height: MediaQuery.of(context).size.height,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20.0),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey,
-                      blurRadius: 5.0,
-                    ),
-                  ]),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(height: 30.0),
-                    // Form to fill in email
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.9,
-                      child: TextFormField(
-                        decoration:
-                            textInputDecoration.copyWith(hintText: 'Email'),
-                        validator: (val) =>
-                            val!.isEmpty ? "Enter an email" : null,
-                        onChanged: (value) {
-                          setState(() => (email = value));
-                        },
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    // Form to fill in password
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.9,
-                      child: TextFormField(
-                        decoration: textInputDecoration.copyWith(
-                          hintText: 'Password',
-                          suffixIcon: IconButton(
-                              onPressed: () {
-                                setState(() {
-                                  obscurePassword = !obscurePassword;
-                                });
-                              },
-                              icon: Icon(Icons.visibility)),
-                          // suffixIcon: Icon(Icons.visibility),
-                        ),
-                        obscureText: obscurePassword,
-                        validator: (val) => val!.length < 6
-                            ? "Enter a password with at least 6 characters"
-                            : null,
-                        onChanged: (value) {
-                          setState(() => (password = value));
-                        },
-                      ),
-                    ),
-                    SizedBox(
-                      height: 50.0,
-                    ),
-                    // Sign in button
-                    SizedBox(
-                      width: 200.0,
-                      height: 50.0,
-                      child: ElevatedButton(
-                          style: ButtonStyle(
-                            shape: MaterialStateProperty.all(
-                                RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30.0),
-                            )),
-                            backgroundColor:
-                                MaterialStateProperty.all(Colors.pink),
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.red[50],
+            body: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 60, 20, 20),
+                    child: Image.asset('assets/Logo.png'),
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey,
+                            blurRadius: 5.0,
                           ),
-                          onPressed: () async {
-                            if (_formKey.currentState!.validate()) {
-                              setState(() => loading = true);
-                              dynamic result =
-                                  await _auth.registerWithEmailAndPassword(
-                                      email, password);
-                              if (result == null) {
-                                setState(() {
-                                  error = 'Please supply a valid email';
-                                  loading = false;
-                                });
-                              }
-                            }
-                          },
-                          child: Text(
-                            'Register',
-                            style: TextStyle(
-                              fontSize: 20.0,
-                              color: Colors.white,
+                        ]),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(height: 30.0),
+                          // Form to fill in email
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.9,
+                            child: TextFormField(
+                              decoration: textInputDecoration.copyWith(
+                                  hintText: 'Email'),
+                              validator: (val) =>
+                                  val!.isEmpty ? "Enter an email" : null,
+                              onChanged: (value) {
+                                setState(() => (email = value));
+                              },
                             ),
-                          )),
-                    ),
-                    Text(
-                      error,
-                      style: TextStyle(
-                        color: Colors.red,
-                        fontSize: 14.0,
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          // Form to fill in password
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.9,
+                            child: TextFormField(
+                              decoration: textInputDecoration.copyWith(
+                                hintText: 'Password',
+                                suffixIcon: IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        obscurePassword = !obscurePassword;
+                                      });
+                                    },
+                                    icon: Icon(Icons.visibility)),
+                                // suffixIcon: Icon(Icons.visibility),
+                              ),
+                              obscureText: obscurePassword,
+                              validator: (val) => val!.length < 6
+                                  ? "Enter a password with at least 6 characters"
+                                  : null,
+                              onChanged: (value) {
+                                setState(() => (password = value));
+                              },
+                            ),
+                          ),
+                          SizedBox(
+                            height: 50.0,
+                          ),
+                          // Sign in button
+                          SizedBox(
+                            width: 200.0,
+                            height: 50.0,
+                            child: ElevatedButton(
+                                style: ButtonStyle(
+                                  shape: MaterialStateProperty.all(
+                                      RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30.0),
+                                  )),
+                                  backgroundColor:
+                                      MaterialStateProperty.all(Colors.pink),
+                                ),
+                                onPressed: () async {
+                                  if (_formKey.currentState!.validate()) {
+                                    try {
+                                      setState(() => loading = true);
+                                      await _auth.registerWithEmailAndPassword(email, password);
+                                    } on FirebaseAuthException catch (e) {
+                                      setState(() {
+                                        error = e.message!;
+                                        loading = false;
+                                      });
+                                    }
+                                  }
+                                },
+                                child: Text(
+                                  'Register',
+                                  style: TextStyle(
+                                    fontSize: 20.0,
+                                    color: Colors.white,
+                                  ),
+                                )),
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            error,
+                            style: TextStyle(
+                              color: Colors.red,
+                              fontSize: 14.0,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text("Already have an account? "),
+                              TextButton(
+                                  onPressed: widget.toggleView,
+                                  child: Text("Login!")),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text("Already have an account? "),
-                        TextButton(
-                            onPressed: widget.toggleView,
-                            child: Text("Login!")),
-                      ],
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
-    );
+          );
   }
 }
